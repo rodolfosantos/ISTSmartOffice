06344cccaa421aced8d808bf1f1fe068fc39e91d
pjcarreira <pjcarreira@gmail.com>
2014-04-02 17:56:49 +0100

    [UPDATE] Minor adjustments do data types

--- src/eu/smartcampus/api/DeviceConnectivityAPI.java
+++ src/eu/smartcampus/api/DeviceConnectivityAPI.java
@@ -7,45 +7,60 @@ import java.security.Timestamp;
 import java.util.List;
 import java.util.Map;
 
-import eu.smartcampus.util.*;
-
+import sun.management.Sensor;
+import eu.smartcampus.util.Client;
+import eu.smartcampus.util.DatapointAddress;
+import eu.smartcampus.util.Metadata;
+import eu.smartcampus.util.Reading;
+import eu.smartcampus.util.SamplingRate;
+import eu.smartcampus.util.SensorId;
+import eu.smartcampus.util.SensorType;
+import eu.smartcampus.util.Timewindow;
 
 /**
  * @author Diogo
- *
  */
-public interface RemoteAPI {
+public interface DeviceConnectivityAPI {
 
     /**
-     * Gets an array with the corresponding sampling rate for the datapoints of a given sensor.
-     *
-     * @param  sensor the sensor to which belong the datapoints
-     * @param  datapoints the list of datapoints for which we want to know the sampling rate
+     * Gets an array with the corresponding sampling rate for the datapoints of a given
+     * sensor.
+     * 
+     * @param sensor the sensor to which belong the datapoints
+     * @param datapoints the list of datapoints for which we want to know the sampling
+     *            rate
      * @return the sampling rate for the corresponding datapoint
      */
-    public Map<DatapointAddress, SamplingRate> getCurrentSamplingRate(Sensor sensor, List<DatapointAddress> datapoints);
-    
-    public List<DatapointAddress> getDataPointAddresses(Sensor sensor);
-    
-    public Map<DatapointAddress, Reading> getLastReading(Sensor sensor, List<DatapointAddress> datapoints);
-    
-    public Map<Timestamp, List<Reading>> readTimeWindow(Sensor sensor, List<DatapointAddress> datapoints, Timewindow window);
-    
-    public Map<Timestamp, List<Reading>> readSizeWindow(Sensor sensor, List<DatapointAddress> datapoints, Integer nReadings);
-            
-    public Map<DatapointAddress, Reading> forceDatapointReading(Sensor sensor, List<DatapointAddress> datapoints);
+    public Map<DatapointAddress, SamplingRate> getCurrentSamplingRate(Sensor sensor,
+                                                                      List<DatapointAddress> datapoints);
 
-    public Map<DatapointAddress, Integer> getReadStorageCapacity(Sensor sensor, List<DatapointAddress> datapoints);
-    
-    public Map<DatapointAddress, Metadata> getSensorMetadata(Sensor sensor, List<DatapointAddress> datapoints);
-    
-    public List<Sensor> getSensorByType(SensorType type);
-    
-    public List<Sensor> getAllSensors();
-    
+    public List<DatapointAddress> getDataPointAddresses(SensorId sensor);
+
+    public Map<DatapointAddress, Reading> getLastReading(SensorId sensor,
+                                                         List<DatapointAddress> datapoints);
+
+    public Map<Timestamp, List<Reading>> readTimeWindow(SensorId sensor,
+                                                        List<DatapointAddress> datapoints,
+                                                        Timewindow window);
+
+    public Map<Timestamp, List<Reading>> readSizeWindow(SensorId sensor,
+                                                        List<DatapointAddress> datapoints,
+                                                        Integer nReadings);
+
+    public Map<DatapointAddress, Reading> forceDatapointReading(SensorId sensor,
+                                                                List<DatapointAddress> datapoints);
+
+    public Map<DatapointAddress, Integer> getReadStorageCapacity(SensorId sensor,
+                                                                 List<DatapointAddress> datapoints);
+
+    public Map<DatapointAddress, Metadata> getSensorMetadata(SensorId sensor,
+                                                             List<DatapointAddress> datapoints);
+
+    public List<SensorId> getSensorByType(SensorType type);
+
+    public List<SensorId> getAllSensors();
+
     public void registerListner(Client client, int idSensor, List<DatapointAddress> datapoints);
-    
+
     public void removeListner(Client client, int idSensor, List<DatapointAddress> datapoints);
 }
-
-

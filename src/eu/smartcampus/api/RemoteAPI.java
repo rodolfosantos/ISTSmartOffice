70f412f1955e6770e21db2adbacc0bb7a006986d
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2014-04-02 16:41:30 +0100

    -metodos da API na interface DONE -Falta: Classes Stub, Comentarios JavaDoc (estou a tratar disso)

--- src/eu/smartcampus/api/RemoteAPI.java
+++ src/eu/smartcampus/api/RemoteAPI.java
@@ -15,14 +15,29 @@ import eu.smartcampus.util.*;
  */
 public interface RemoteAPI {
 
-    public Map<DatapointAddress,SamplingRate> getCurrentSamplingRate(int idSensor, List<DatapointAddress> datapoints);
+    public Map<DatapointAddress, SamplingRate> getCurrentSamplingRate(int idSensor, List<DatapointAddress> datapoints);
     
     public List<DatapointAddress> getDataPointAddresses(int idSensor);
     
-    public Map<DatapointAddress,LastReading> getLastReading(int idSensor, List<DatapointAddress> datapoints);
+    public Map<DatapointAddress, Reading> getLastReading(int idSensor, List<DatapointAddress> datapoints);
     
-    public Map<> readTimeWindow(int idSensor, List<DatapointAddress> datapoints, Timespan delta);
+    public Map<Timestamp, List<Reading>> readTimeWindow(int idSensor, List<DatapointAddress> datapoints, Timewindow window);
     
+    public Map<Integer, List<Reading>> readSizeWindow(int idSensor, List<DatapointAddress> datapoints, Timewindow nReadings);
+            
+    public Map<DatapointAddress, Reading> forceDatapointReading(int idSensor, List<DatapointAddress> datapoints);
+
+    public Map<DatapointAddress, Integer> getReadStorageCapacity(int idSensor, List<DatapointAddress> datapoints);
     
+    public Map<DatapointAddress, Metadata> getSensorMetadata(int idSensor, List<DatapointAddress> datapoints);
     
+    public List<idSensor> getSensorByType(SensorType type);
+    
+    public List<idSensor> getAllSensors();
+    
+    public void registerListner(Client client, int idSensor, List<DatapointAddress> datapoints);
+    
+    public void removeListner(Client client, int idSensor, List<DatapointAddress> datapoints);
 }
+
+

2e124cb1957c89b734b5df46847c1d1d314653f1
PedroD <pedro.domingues@ist.utl.pt>
2014-05-12 01:29:28 +0100

    Restlet activator created.

--- eu.smartcampus.api.deviceconnectivity.wrappers.rest/src/eu/smartcampus/api/deviceconnectivity/wrappers/rest/Activator.java
+++ eu.smartcampus.api.deviceconnectivity.wrappers.rest/src/eu/smartcampus/api/deviceconnectivity/wrappers/rest/Activator.java
@@ -2,27 +2,33 @@ package eu.smartcampus.api.deviceconnectivity.wrappers.rest;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
+import org.restlet.Component;
+import org.restlet.data.Protocol;
 
 public final class Activator implements BundleActivator {
 
-	private ServiceRegistration registration;
+	private Component component;
 
 	@Override
 	public void start(BundleContext context) throws Exception {
-		DatapointConnectivityServiceRESTWrapper restWrapper = DatapointConnectivityServiceRESTWrapper
-				.getInstance();
-		if (registration == null) {
-			registration = context.registerService(
-					DatapointConnectivityServiceRESTWrapper.class.getName(),
-					restWrapper, null);
-		}
+		// Create a new Component.
+		Component component = new Component();
+
+		// Add a new HTTP server listening on port 8182.
+		component.getServers().add(Protocol.HTTP, 8182);
+
+		// Attach device api application
+		
+		component.getDefaultHost().attach("/deviceconnectivityapi",
+				DatapointConnectivityServiceRESTWrapper.getInstance());
+
+		// Start the component.
+		component.start();
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
-		registration.unregister();
-		registration = null;
+		component.stop();
 	}
 
 }

615168022e77eac6729e0d92452cf357a4c1b898
Rodolfo Santos <rodolfoahsantos@gmail.com>
2014-05-14 03:02:59 +0100

    Created History Data Storage API project scaffold.

---
+++ eu.smartcampus.api.historydatastorage/src/eu/smartcampus/api/historydatastorage/IHistoryDataStorageService.java
@@ -0,0 +1,9 @@
+package eu.smartcampus.api.historydatastorage;
+
+public interface IHistoryDataStorageService {
+	
+	String getImplementationName();
+	
+	// TODO
+
+}
---
+++ eu.smartcampus.api.historydatastorage/src/eu/smartcampus/api/historydatastorage/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Java service definitions for the History Data Storage API.
+ */
+package eu.smartcampus.api.historydatastorage;

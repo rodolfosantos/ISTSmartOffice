db47b928159347c1cfad1a21a7429b4dee944e2f
Rodolfo Santos <rodolfoahsantos@gmail.com>
2014-05-14 03:25:09 +0100

    Some tentative History Data Storage API methods.

--- eu.smartcampus.api.historydatastorage/src/eu/smartcampus/api/historydatastorage/IHistoryDataStorageService.java
+++ eu.smartcampus.api.historydatastorage/src/eu/smartcampus/api/historydatastorage/IHistoryDataStorageService.java
@@ -1,9 +1,16 @@
 package eu.smartcampus.api.historydatastorage;
 
 public interface IHistoryDataStorageService {
-	
+
 	String getImplementationName();
-	
-	// TODO
+
+	// TODO improve
+
+	void addValue(String address, long timestamp, String value);
+
+	String getLastValue(String address);
+
+	String[] getValuesTimeWindow(String address, long startTimestamp,
+			long finishTimestamp);
 
 }

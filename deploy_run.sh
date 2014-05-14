6d9126977f0e18450b8370e5538ca407bc9d7920
Rodolfo Santos <rodolfoahsantos@gmail.com>
2014-05-14 03:09:12 +0100

    Bash script to deploy project on server.

---
+++ deploy_run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#remove previews folder
+sudo rm -r smartcampusapi
+
+#download source code
+git clone https://github.com/it4energy/smartcampusapi.git
+cd smartcampusapi
+
+#download apache felix
+wget http://mirrors.fe.up.pt/pub/apache//felix/org.apache.felix.main.distribution-4.4.0.zip
+unzip org.apache.felix.main.distribution-4.4.0.zip
+
+#clean
+mvn clean
+#install
+mvn install
+#generate bundles dependencies
+mvn -Pcreate-osgi-bundles-from-dependencies bundle:wrap
+
+#copy bundles to felix framework bundle folder
+find . -iname *.jar -exec cp {} felix-framework-4.4.0/bundle/ \;
+
+cd felix-framework-4.4.0
+
+#run apache felix
+java -Dgosh.args=--nointeractive -jar bin/felix.jar
+

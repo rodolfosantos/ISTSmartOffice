3637b42dfb4c05e314d50331717245ce08180b43
Rodolfo Santos <rodolfoahsantos@gmail.com>
2014-05-29 19:32:37 +0100

    [LIFX] Working (only on/off)

--- eu.smartcampus.api.deviceconnectivity.impls.lifx/src/eu/smartcampus/api/deviceconnectivity/impls/lifx/Activator.java
+++ eu.smartcampus.api.deviceconnectivity.impls.lifx/src/eu/smartcampus/api/deviceconnectivity/impls/lifx/Activator.java
@@ -1,41 +1,52 @@
 package eu.smartcampus.api.deviceconnectivity.impls.lifx;
 
-import java.util.Collection;
-
-import jlifx.bulb.DiscoveryService;
-import jlifx.bulb.GatewayBulb;
-import jlifx.bulb.IBulb;
-
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+
+import eu.smartcampus.api.deviceconnectivity.IDatapointConnectivityService;
+import eu.smartcampus.api.deviceconnectivity.osgi.registries.DeviceConnectivityServiceRegistry;
 
 public final class Activator implements BundleActivator {
 
 	@Override
 	public void start(BundleContext context) throws Exception {
 
-		System.out.println("Discovering LIFX gateway bulb...");
-        GatewayBulb gatewayBulb = DiscoveryService.discoverGatewayBulb();
-        if (gatewayBulb == null) {
-        	System.out.println("No LIFX gateway bulb found!");
-        	System.out.println("");
-        } else {
-        	System.out.println("Found LIFX gateway bulb:");
-        	System.out.println("IP address  : " + gatewayBulb.getInetAddress().getHostAddress());
-        	System.out.println("MAC address : " + gatewayBulb.getMacAddressAsString());
-            Collection<IBulb> allBulbs = DiscoveryService.discoverAllBulbs(gatewayBulb);
-            System.out.println("Found " + allBulbs.size() + " bulb(s) in network:");
-            for (IBulb bulb : allBulbs) {
-            	System.out.println("Bulb name   : " + bulb.getName());
-            	System.out.println("MAC address : " + bulb.getMacAddressAsString());
-            }
-        }
+		// System.out.println("Discovering LIFX gateway bulb...");
+		// GatewayBulb gatewayBulb = DiscoveryService.discoverGatewayBulb();
+		// if (gatewayBulb == null) {
+		// System.out.println("No LIFX gateway bulb found!");
+		// System.out.println("");
+		// } else {
+		// System.out.println("Found LIFX gateway bulb:");
+		// System.out.println("IP address  : " +
+		// gatewayBulb.getInetAddress().getHostAddress());
+		// System.out.println("MAC address : " +
+		// gatewayBulb.getMacAddressAsString());
+		// Collection<IBulb> allBulbs =
+		// DiscoveryService.discoverAllBulbs(gatewayBulb);
+		// System.out.println("Found " + allBulbs.size() +
+		// " bulb(s) in network:");
+		// for (IBulb bulb : allBulbs) {
+		// System.out.println("Bulb name   : " + bulb.getName());
+		// System.out.println("MAC address : " + bulb.getMacAddressAsString());
+		// }
+		// }
+
+		// Create service implementation
+		IDatapointConnectivityService serviceImpl = new DatapointConnectivityServiceLifxDriver();
+		// Publish Service
+		DeviceConnectivityServiceRegistry.getInstance().addService(
+				DatapointConnectivityServiceLifxDriver.class.getName(),
+				serviceImpl);
+		System.out.println("Lifx Started!");
 
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
-		
+		DeviceConnectivityServiceRegistry.getInstance().removeService(
+				DatapointConnectivityServiceLifxDriver.class.getName());
+		System.out.println("Lifx Stopped!");
 	}
 
 }
---
+++ eu.smartcampus.api.deviceconnectivity.impls.lifx/src/eu/smartcampus/api/deviceconnectivity/impls/lifx/DatapointConnectivityServiceLifxDriver.java
@@ -0,0 +1,128 @@
+package eu.smartcampus.api.deviceconnectivity.impls.lifx;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import jlifx.bulb.IBulb;
+import jlifx.commandline.Utils;
+import eu.smartcampus.api.deviceconnectivity.DatapointAddress;
+import eu.smartcampus.api.deviceconnectivity.DatapointMetadata;
+import eu.smartcampus.api.deviceconnectivity.DatapointMetadata.AccessType;
+import eu.smartcampus.api.deviceconnectivity.DatapointMetadata.Datatype;
+import eu.smartcampus.api.deviceconnectivity.DatapointMetadata.MetadataBuilder;
+import eu.smartcampus.api.deviceconnectivity.DatapointReading;
+import eu.smartcampus.api.deviceconnectivity.DatapointValue;
+import eu.smartcampus.api.deviceconnectivity.IDatapointConnectivityService;
+
+public class DatapointConnectivityServiceLifxDriver implements
+		IDatapointConnectivityService {
+
+	private LifxGateway gateway;
+
+	public DatapointConnectivityServiceLifxDriver() {
+		super();
+		this.gateway = new LifxGateway();
+		this.gateway.start();
+	}
+
+	@Override
+	public String getImplementationName() {
+		return "LIFX";
+	}
+
+	@Override
+	public void addDatapointListener(DatapointListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public DatapointAddress[] getAllDatapoints() {
+		Map<String, IBulb> allDevices = gateway.getAllDevices(false);
+
+		DatapointAddress[] result = new DatapointAddress[allDevices.size()];
+
+		Iterator<String> it = allDevices.keySet().iterator();
+		int i = 0;
+		while (it.hasNext()) {
+			String datapointAddress = it.next();
+			result[i++] = new DatapointAddress(datapointAddress);
+		}
+		return result;
+	}
+
+	@Override
+	public DatapointMetadata getDatapointMetadata(DatapointAddress address)
+			throws OperationFailedException {
+		 MetadataBuilder m = new DatapointMetadata.MetadataBuilder();
+		 m.setAccessType(AccessType.READ_WRITE);
+		 m.setDatatype(Datatype.BOOLEAN);
+		 return m.build();		
+	}
+
+	@Override
+	public void removeDatapointListener(DatapointListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int requestDatapointRead(DatapointAddress address,
+			ReadCallback readCallback) {
+		System.out.println(address);
+		Map<String, IBulb> allDevices = gateway.getAllDevices(false);
+		System.out.println(allDevices.entrySet().toString());
+		if (allDevices.containsKey(address.getAddress())) {
+			IBulb bulb = allDevices.get(address.getAddress());
+			String res = "";
+			res += "\nHue         : " + Utils.wordToHexString(bulb.getHue());
+			res += "\nSaturation  : " + Utils.wordToHexString(bulb.getSaturation());
+			res += "\nBrightness  : " + Utils.wordToHexString(bulb.getBrightness());
+			res += "\nKelvin      : " + Utils.wordToHexString(bulb.getKelvin());
+			res += "\nDim         : " + Utils.wordToHexString(bulb.getDim());
+			res += "\nPower       : " + Utils.wordToHexString(bulb.getPower());
+			
+			
+			DatapointReading r = new DatapointReading(new DatapointValue(res));
+			readCallback.onReadCompleted(address, new DatapointReading[] { r },
+					0);
+		} else {
+			readCallback.onReadAborted(address, ErrorType.DATAPOINT_NOT_FOUND, 0);
+		}
+
+		return 0;
+	}
+
+	@Override
+	public int requestDatapointWindowRead(DatapointAddress address,
+			long startTimestamp, long finishTimestamp, ReadCallback readCallback) {
+		return 0;
+	}
+
+	@Override
+	public int requestDatapointWrite(DatapointAddress address,
+			DatapointValue[] values, WriteCallback writeCallback) {
+
+		if (values.length == 1) {
+			DatapointValue value = values[0];
+			if (value.getBooleanValue()) {
+				gateway.switchOn(address.getAddress());
+				writeCallback.onWriteCompleted(address,
+						WritingConfirmationLevel.DEVICE_ACTION_TAKEN, 0);
+			} else {
+				gateway.switchOff(address.getAddress());
+				writeCallback.onWriteCompleted(address,
+						WritingConfirmationLevel.DEVICE_ACTION_TAKEN, 0);
+			}
+		} else if (values.length == 3) {
+			gateway.setColor(address.getAddress(), values[0].getStringValue(),
+					values[1].getIntValue(), values[2].getStringValue());
+		}
+
+		return 0;
+	}
+
+}

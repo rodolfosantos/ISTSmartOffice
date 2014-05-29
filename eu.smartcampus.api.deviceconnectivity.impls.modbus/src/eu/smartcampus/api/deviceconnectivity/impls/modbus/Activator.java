c7696ab3f99d57d43a0e68374b6273e600ffd461
Rodolfo Santos <rodolfoahsantos@gmail.com>
2014-05-29 16:06:51 +0100

    [Modbus] Added modbus implementation.

---
+++ eu.smartcampus.api.deviceconnectivity.impls.modbus/src/eu/smartcampus/api/deviceconnectivity/impls/modbus/Activator.java
@@ -0,0 +1,54 @@
+package eu.smartcampus.api.deviceconnectivity.impls.modbus;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import eu.smartcampus.api.deviceconnectivity.IDatapointConnectivityService;
+import eu.smartcampus.api.deviceconnectivity.impls.modbus.master.ModbusMasterLib;
+import eu.smartcampus.api.deviceconnectivity.osgi.registries.DeviceConnectivityServiceRegistry;
+import eu.smartcampus.api.historydatastorage.HistoryDataStorageServiceImpl;
+import eu.smartcampus.api.historydatastorage.osgi.registries.HistoryDataStorageServiceRegistry;
+import eu.smartcampus.api.osgi.registries.IServiceRegistry.ServiceRegistryListener;
+
+public final class Activator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		// Create service implementation
+		IDatapointConnectivityService serviceImpl = new DatapointConnectivityServiceModbus();
+		// Publish Service
+		DeviceConnectivityServiceRegistry.getInstance().addService(
+				DatapointConnectivityServiceModbus.class.getName(),
+				serviceImpl);
+		
+		
+		final int slaveId = 2;
+        final int registerId = 10;
+        final int nRegisters = 6;
+
+        try {
+        	ModbusMasterLib emu = new ModbusMasterLib();
+            emu.createModbusTcpMaster();
+            for (int i = 0; i < nRegisters + 1; i++) {
+                emu.writeCoil(slaveId, registerId + i, true);
+            }
+            emu.readCoils(slaveId, registerId, nRegisters);
+            emu.destroyModbusMaster();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        
+		
+		System.out.println("Modbus Started!");
+		
+		
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		DeviceConnectivityServiceRegistry.getInstance().removeService(
+				DatapointConnectivityServiceModbus.class.getName());
+		System.out.println("Mosbus Stopped!");
+	}
+
+}
---
+++ eu.smartcampus.api.deviceconnectivity.impls.modbus/src/eu/smartcampus/api/deviceconnectivity/impls/modbus/master/ModbusMasterEmu.java
@@ -0,0 +1,38 @@
+package eu.smartcampus.api.deviceconnectivity.impls.modbus.master;
+
+
+
+/**
+ * The Class ModbusMasterEmu.
+ */
+public final class ModbusMasterEmu {
+
+
+    /**
+     * Instantiates a new modbus master emu.
+     */
+    private ModbusMasterEmu() {
+    }
+
+    /**
+     * The main method.
+     * 
+     * @param args the arguments
+     */
+    public static void main(String[] args) {
+
+        final int slaveId = 2;
+        final int registerId = 10;
+        final int nRegisters = 6;
+
+        ModbusMasterLib emu = new ModbusMasterLib();
+        emu.createModbusTcpMaster();
+        for (int i = 0; i < nRegisters + 1; i++) {
+            emu.writeCoil(slaveId, registerId + i, true);
+        }
+        emu.readCoils(slaveId, registerId, nRegisters);
+        emu.destroyModbusMaster();
+
+    }
+
+}

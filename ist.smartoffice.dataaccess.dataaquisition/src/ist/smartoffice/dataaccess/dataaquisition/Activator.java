f28aa78d5715234856e46c7862b3db28503e02b0
Rodolfo Santos <rodolfoahsantos@gmail.com>
2014-06-12 14:59:59 +0100

    Added data access service bundles (scaffold)

---
+++ ist.smartoffice.dataaccess.dataaquisition/src/ist/smartoffice/dataaccess/dataaquisition/Activator.java
@@ -0,0 +1,28 @@
+package ist.smartoffice.dataaccess.dataaquisition;
+
+import ist.smartoffice.logger.Logger;
+import ist.smartoffice.logger.LoggerService;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public final class Activator implements BundleActivator {
+
+	private static final String SERVICE_NAME = Activator.class
+			.getName();//TODO
+
+	static private Logger log = LoggerService.getInstance().getLogger(
+			Activator.class.getName());
+
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+
+	}
+
+}
